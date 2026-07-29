76cbd4095a577903feca483267ff4ebde84e906b
XiaotianMC <BilibiliXiaotian@outlook.com>
2026-07-29 21:20:03 +0800

    Initial Commit

---
+++ src/client/java/cn/gcte/client/LegacyEndRingsClient.java
@@ -0,0 +1,10 @@
+package cn.gcte.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LegacyEndRingsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
